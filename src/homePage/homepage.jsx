42aeac25be307cf9caf7fbdd9e09ec4f13bae710
pankaj sondhiya <pankajsondhiya780@gamil.com>
2024-01-07 23:25:58 +0530

    final build2

--- src/homePage/homepage.jsx
+++ src/homePage/homepage.jsx
@@ -94,7 +94,7 @@ export default function HomePage({ coins, currency }) {
                   <td className="p-3">
                     <div className="coinInfo d-flex text-white">
                       <div className="coinImg">
-                        <img src={coin.image} alt="" />
+                        <img src={coin.image} alt="" className="coin_img" />
                       </div>
                       <div className="basicInfo d-flex">
                         <div className="symbol ">
